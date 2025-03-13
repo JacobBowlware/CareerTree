0b3f8c96e93d5d45b32a82de9857f7d4a4ded8c8
Jacob Bowlware <jacobbowlware@gmail.com>
2025-03-13 17:38:51 -0500

    Modify review section

--- CareerPathApp/src/app/features/home/home.component.ts
+++ CareerPathApp/src/app/features/home/home.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  
+  counter(n: number): any[] {
+    return new Array(n).fill(1);
+  }
 }
