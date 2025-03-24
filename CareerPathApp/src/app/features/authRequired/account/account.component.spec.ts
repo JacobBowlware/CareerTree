989c9f9d842dca7aa001f0cd08e5d45cd3187be8
Jacob Bowlware <jacobbowlware@gmail.com>
2025-03-23 20:54:33 -0500

    Improve overall CSS styling

--- CareerPathApp/src/app/features/authRequired/account/account.component.spec.ts
+++ CareerPathApp/src/app/features/authRequired/account/account.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StartComponent } from './start.component';
+import { AccountComponent } from './account.component';
 
-describe('StartComponent', () => {
-  let component: StartComponent;
-  let fixture: ComponentFixture<StartComponent>;
+describe('AccountComponent', () => {
+  let component: AccountComponent;
+  let fixture: ComponentFixture<AccountComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StartComponent]
+      imports: [AccountComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(StartComponent);
+    fixture = TestBed.createComponent(AccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- CareerPathApp/src/app/features/home/home.component.ts
+++ CareerPathApp/src/app/features/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -10,6 +11,12 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private router: Router) {}
+
+  handleGetStarted(): void {
+    this.router.navigate(['/signup']);
+  }
   
   counter(n: number): any[] {
     return new Array(n).fill(1);
