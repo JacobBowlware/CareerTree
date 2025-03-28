596c874ea8083739f352ac22ec1c21207e5840bd
Jacob Bowlware <jacobbowlware@gmail.com>
2025-03-27 19:27:11 -0500

    Change font-family, update home text

---
+++ CareerPathApp/src/app/features/authRequired/newUserFlow/skill-analysis/skill-analysis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-analysis',
+  imports: [],
+  templateUrl: './skill-analysis.component.html',
+  styleUrl: './skill-analysis.component.scss'
+})
+export class SkillAnalysisComponent {
+
+}
